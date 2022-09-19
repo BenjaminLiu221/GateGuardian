60a52db794cf9eabf2e02f48e20f40a896111b7b
Benjamin Liu <benjaminliu221@gmail.com>
2022-09-19 01:23:41 -0700

    remove yelp

---
+++ GateGuardian/GateGuardianWeb/Controllers/YelpController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GateGuardianWeb.Controllers
+{
+    public class YelpController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet("name = Businesses")]
+        public async IActionResult GetAllBusinesses()
+        {
+            var request = new HttpRequestMessage(HttpMethod.Get, "https://api.yelp.com/v3/businesses/search?location=SanFrancisco");
+
+        }
+    }
+}
